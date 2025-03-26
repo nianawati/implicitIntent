e4738c0bab9d06ba30e58e1b3c54001936fcb92c
nianawati <mudhiyanianawati55754@gmail.com>
2025-03-26 13:58:34 +0700

    update
    update implicitIntence

---
+++ app/src/main/java/com/mudhiya/implicitintent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.mudhiya.implicitintent;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void tampilTelepon (View view) {
+        Intent teleponIntent = new Intent(Intent.ACTION_DIAL);
+        startActivity(teleponIntent);
+    }
+
+
+
+
+}
